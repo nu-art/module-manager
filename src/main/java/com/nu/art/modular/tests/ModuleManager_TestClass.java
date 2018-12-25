301375f644f35188b35b37b7c75d1567f0c4aef4
TacB0sS <adam.vdk@nu-art-software.com>
2018-12-25 10:39:44 +0200

    separated transceiver base from cyborg and create io lib, started working on google cloud storage

---
+++ src/main/java/com/nu/art/modular/tests/ModuleManager_TestClass.java
@@ -0,0 +1,37 @@
+package com.nu.art.modular.tests;
+
+import com.nu.art.belog.BeLogged;
+import com.nu.art.belog.DefaultLogClient;
+import com.nu.art.belog.Logger;
+import com.nu.art.modular.core.Module;
+import com.nu.art.modular.core.ModuleManager;
+import com.nu.art.modular.core.ModuleManagerBuilder;
+import com.nu.art.modular.core.ModulesPack;
+
+public class ModuleManager_TestClass
+	extends Logger {
+
+	public static ModuleManager moduleManager;
+
+	@SuppressWarnings("unchecked")
+	public static void initWithModules(Class<? extends Module>... moduleTypes) {
+		if (moduleManager != null)
+			return;
+
+		BeLogged.getInstance().addClient(new DefaultLogClient());
+		moduleManager = new ModuleManagerBuilder().addModules(moduleTypes).build();
+	}
+
+	@SuppressWarnings("unchecked")
+	public static void initWithPacks(Class<? extends ModulesPack>... packs) {
+		if (moduleManager != null)
+			return;
+
+		BeLogged.getInstance().addClient(new DefaultLogClient());
+		moduleManager = new ModuleManagerBuilder().addModulePacks(packs).build();
+	}
+
+	public static <ModuleType extends Module> ModuleType getModule(Class<ModuleType> moduleType) {
+		return moduleManager.getModule(moduleType);
+	}
+}
